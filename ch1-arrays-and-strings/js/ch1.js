15e40051362030a3314fefe96168df1273029966
jayunani <j.ayunani@gmail.com>
2016-08-07 19:27:57 -0700

    added more tests until challenge 1.6

--- ch1-arrays-and-strings/js/ch1.js
+++ ch1-arrays-and-strings/js/ch1.js
@@ -11,20 +11,8 @@ function uniqueChar(str) {
 	return true;
 }
 
-// CHALLENGE 1.2 
-// Implement a function which reverses a string
 
-function reverseString(str) {
-	if(typeof str !== "string") return null;
-	var reversed = "";
-	var tempLetterHolder = "";
-	for(var i = str.length-1; i >= 0; i--) {
-		reversed += str[i];
-	}
-	return reversed;
-}
-
-// CHALLENGE 1.3
+// CHALLENGE 1.2
 // Given two strings, write a method to decide if one is a permutation of the other.
 
 function isPerm(str1, str2) {
@@ -32,4 +20,74 @@ function isPerm(str1, str2) {
 	if(typeof str1 !== "string" || typeof str2 !== "string") return false;
 	if(str1.split("").sort().join("") === str2.split("").sort().join("")) return true;
 	return false;
+}
+
+// CHALLENGE 1.3
+// Write a method to replace all spaces in a string with '%20'. You may assume that the
+// string has sufficient space at the end to hold the additional characters, and that you 
+// are given the 'true' length of the string.
+// input: "Mr John Smith     ", 13
+// output: "Mr%20John%20Smith"
+
+function urlify(str, num) {
+
+}
+
+// CHALLENGE 1.4
+// Given a string, write a function to check if it is a permutation of a palindrome. 
+// A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+// is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+// input: 'tact coa'
+// output: 'true' (permutations: 'taco cat', 'atco cta', etc.)
+
+function palindromePerm(str) {
+
+}
+
+// CHALLENGE 1.5
+// There are three types of edits that can be performed on strings: insert a character,
+// remove a character, or replace a character. Given two strings, write a function to check 
+// if they are one edit(or zero edits) away.
+// pale, ple = true
+// pales, pale = true
+// pale, bake = false
+
+function oneAway(str1, str2) {
+
+}
+
+// CHALLENGE 1.6 
+// Implement a method to perform basic string compression using the counts of repeated characters/
+// For example, the string 'aabcccccaaa' would become 'a2b1c5a3'. If the compressed string 
+// would not become smaller than the original string, the method should return the original string.
+// You can assume the string has only uppercase and lowercase letters (a-z).
+
+function strCompression(str) {
+
+}
+
+// CHALLENGE 1.7
+// Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+// write a method to rotate the image by 90 degrees. Can you do this in place?
+
+function rotateMatrix(matrix) {
+
+}
+
+// CHALLENGE 1.8 
+// Write an algorithm such that if an element in an MxN matrix is 0, its entire row
+// and column are set to 0.
+
+function zeroMatrix(matrix) {
+
+}
+
+// CHALLENGE 1.9
+// Assume you have a method isSubstring which checks if one word is a substring of another.
+// Given two strings, str1 and str2, write code to check if s2 is a rotation of s1 using only 
+// one call to isSubstring.
+// e.g. 'waterbottle' is a rotation of 'erbottlewat'
+
+function stringRotation(str1, str2) {
+
 }
--- ch1-arrays-and-strings/test/ch1test.js
+++ ch1-arrays-and-strings/test/ch1test.js
@@ -1,7 +1,7 @@
 var expect = chai.expect;
 
 describe('Chapter 1 Challenges...', function() {
-	describe('challenge 1.1 unique characters', function() {
+	describe('challenge 1.1 Is Unique', function() {
 		it('should return true if all characters are unique', function() {
 			expect(uniqueChar('cat')).to.eql(true);
 			expect(uniqueChar('map')).to.eql(true);
@@ -16,17 +16,7 @@ describe('Chapter 1 Challenges...', function() {
 			expect(uniqueChar([])).to.eql(false);
 		});
 	});
-	describe('challenge 1.2 reverse', function() {
-		it('should return the string input reversed', function() {
-			expect(reverseString('cat')).to.eql('tac');
-			expect(reverseString('canada')).to.eql('adanac');
-		});
-		it('should return null if input is not a string', function() {
-			expect(reverseString(76)).to.eql(null);
-			expect(reverseString({})).to.eql(null);
-		});
-	});
-	describe('challenge 1.3 permutation', function() {
+	describe('challenge 1.2 Check Permutation', function() {
 		it('should return true if string 2 is a permutation of string 1', function() {
 			expect(isPerm('cat', 'tac')).to.eql(true);
 			expect(isPerm('target', 'tegrat')).to.eql(true);
@@ -38,6 +28,53 @@ describe('Chapter 1 Challenges...', function() {
 		it('should return false if either inputs are not strings', function() {
 			expect(isPerm('1', 'cat')).to.eql(false);
 			expect(isPerm('82', [])).to.eql(false);
+		});
+	describe('challenge 1.3 URLify', function() {
+		it('should return a string with %20 in place of spaces', function() {
+			expect(urlify('the dog and cat', 15)).to.eql('the%20dog%20and%20cat');
+			expect(urlify('jessica ayunani', 15)).to.eql('jessica%20ayunani');
+		});
+		it('should return a string that is the length of the number input', function() {
+			expect(urlify('the cat   ', 7)).to.eql('the%20cat');
+			expect(urlify('   hi there')).to.eql('hi%20there');
+		});
+	});
+	describe('challenge 1.4 Palindrome Permutation', function() {
+		it('should return true if the string is a permutation of a palindrome', function() {
+			expect(palindromePerm('tact coa')).to.eql(true);
+			expect(palindromePerm('aaccrre')).to.eql(true);
+		});
+		it('should return false in all other cases', function() {
+			expect(palindromePerm('racecars')).to.eql(false);
+			expect(palindromePerm('jessica')).to.eql(false);
+		});
+	});
+	describe('challenge 1.5 One Away', function() {
+		it('should return true if str2 is one character removed from str1', function() {
+			expect(oneAway('pale', 'ple')).to.eql(true);
+			expect(oneAway('dogs', 'dgs')).to.eql(true);
+		});
+		it('should return true if str2 is str1 plus one more character', function() {
+			expect(oneAway('marker', 'markers')).to.eql(true);
+			expect(oneAway('lympics', 'olympics')).to.eql(true);
+		});
+		it('should return true if str2 is str1 with one letter replaced', function() {
+			expect(oneAway('dogs', 'logs')).to.eql(true);
+			expect(oneAway('abcd', 'abcc')).to.eql(true);
+		});
+		it('should return false for all other cases', function() {
+			expect(oneAway('pale', 'bake')).to.eql(false);
+			expect(oneAway('hello', 'hi')).to.eql(false);
+		});
+	});
+	describe('challenge 1.6 String Compression', function() {
+		it('should return a compressed version of a string with counts of letters', function() {
+			expect(strCompression('hhheeeyyyy')).to.eql('h3e3y4');
+			expect(strCompression('aabcccccaaa')).to.eql('a2b1c5a3');
+		});
+		it('should return the original string if the compressed string is longer than the original string', function() {
+			expect(strCompression('dogss')).to.eql('dogss');
+			expect(strCompression('jessica')).to.eql('jessica');
 		});
 	});
 });
